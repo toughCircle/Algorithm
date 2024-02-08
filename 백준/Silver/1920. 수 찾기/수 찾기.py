c84b62f0d5faf640f60c7aa7a8048d0b1c9978fc
toughCircle <150303834+toughCircle@users.noreply.github.com>
2024-02-08 10:28:40 +0900

    [Silver IV] Title: 수 찾기, Time: 164 ms, Memory: 50540 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = set(map(int, input().split()))
+
+M = int(input())
+B = list(map(int, input().split()))
+
+for i in B:
+    if i in A:
+        print(1)
+    else:
+        print(0)

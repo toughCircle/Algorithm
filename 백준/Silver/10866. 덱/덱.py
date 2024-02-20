3b98b1947a4ad98abc5049aca7ec231360e21c65
toughCircle <150303834+toughCircle@users.noreply.github.com>
2024-02-20 10:31:32 +0900

    [Silver IV] Title: 덱, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,47 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+deque = []
+
+for i in range(N) :
+    command = input().split()
+
+    if command[0] == "push_front":
+        deque.insert(0, command[1])
+    
+    if command[0] == "push_back":
+        deque.append(command[1])
+
+    if command[0] == "pop_front":
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque.pop(0))
+    
+    if command[0] == "pop_back":
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque.pop())
+
+    if command[0] == "size":
+        print(len(deque))
+
+    if command[0] == "empty":
+        if len(deque) == 0:
+            print(1)
+        else:
+            print(0)
+
+    if command[0] == "front":
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[0])
+
+    if command[0] == "back":
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[-1])

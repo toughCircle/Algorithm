39ced3743b1e044cf24cac17d5e1f5935c330647
toughCircle <150303834+toughCircle@users.noreply.github.com>
2024-03-05 09:42:49 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+coin = [int(input()) for _ in range(N)]
+
+coin.reverse()
+
+count = 0
+
+while M != 0:
+    for i in coin:
+        if i <= M:
+            count += M // i
+            M = M % i
+        if M == 0:
+            break
+
+
+print(count)

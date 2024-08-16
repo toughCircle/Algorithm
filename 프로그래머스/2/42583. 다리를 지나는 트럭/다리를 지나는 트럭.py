4b1365a292cec828f006b53dc72d89db4edad4b4
toughCircle <150303834+toughCircle@users.noreply.github.com>
2024-08-17 01:27:53 +0900

    코드 수정

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -20,10 +20,12 @@ def solution(bridge_length, weight, truck_weights):
 
         # 다리 위 차 무게 합 + 현재 대기 차 무게 <= 최대 무게
         if sum(b) + i <= weight:
+            # 출차 후 빈 자리에 현재 대기 차 추가, 1초 증가
             if len(b) == bridge_length - 1:
                 b.append(i)
                 answer += 1
                 continue
+            # 출차가 아닌 경우
             else:
                 b.popleft()
                 b.append(i)
@@ -42,7 +44,7 @@ def solution(bridge_length, weight, truck_weights):
                 while b[0] == 0:
                     b.append(b.popleft())
                     answer += 1
-            # 가장 앞 차가 존재하면 출차, 1초 증가
+            # 가장 앞 차가 존재하면 출차 후 현재 대기 차 다시 대기
             b.popleft()
             t.appendleft(i)
             continue 
